263ec037dd9aadcdb0eb540d658b320d86807560
vissernaomiwm@gmail.com <vissernaomiwm@gmail.com>
2024-11-06 00:12:45 +0100

    added a new feature

--- tab3.py
+++ tab3.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from pathlib import Path
-import plotly.express as px
-import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import datetime
 import folium
 from streamlit_folium import st_folium
-import matplotlib.pyplot as plt
-import matplotlib.colors
 
 def load_tab3():
 
@@ -21,34 +13,30 @@ def load_tab3():
     @st.cache_data
     def load_data(bestandsnaam, scheidingsteken): 
         try:
-            schedule = pd.read_csv(bestandsnaam, sep=scheidingsteken)  # Lees het CSV-bestand
-            return schedule  # Retourneer de DataFrame
+            schedule = pd.read_csv(bestandsnaam, sep=scheidingsteken) 
+            return schedule
         except FileNotFoundError:
-            st.error(f'Bestand {bestandsnaam} niet gevonden.') 
+            st.error(f'Bestand {bestandsnaam} niet gevonden.')
             return None
         except Exception as e:
             st.error(f'Fout bij het lezen van {bestandsnaam}: {e}')
-            return None  # Retourneer None als er een fout is
+            return None
 
     @st.cache_data
-    def load_excel(bestandsnaam):
+    def load_excel(bestandsnaam):  
         try:
             df = pd.read_excel(bestandsnaam)
-            return df  # Retourneer de DataFrame
+            return df
         except FileNotFoundError:
-            print(f'Bestand {bestandsnaam} niet gevonden.') 
+            st.error(f'Bestand {bestandsnaam} niet gevonden.')
+            return None
         except Exception as e:
-            print(f'Fout bij het lezen van {bestandsnaam}: {e}')
-        return None  # Retourneer None als er een fout is
+            st.error(f'Fout bij het lezen van {bestandsnaam}: {e}')
+            return None
 
-    # functie om data te cleane
-    @st.cache_data
-    def clean_data(df):
-        return df.dropna(subset=['Time (secs)', '[3d Altitude M]'])
-
-    # laad de data
+    # Laad de data met relevante kolommen en dtypes
     df_airports = load_data('airports-extended-clean.csv', ';')
-    dfschema = load_data('schedule_airport.csv', ',')
+    dfschema = load_data('schedule_airport.csv',',')
 
     # clean de data
     df_airports['ICAO'].dropna(inplace=True)
@@ -68,6 +56,7 @@ def load_tab3():
     unique_icao_orgdes['Longitude'] = unique_icao_orgdes['Longitude'].str.replace(',', '.').astype(float)
 
     merge['FLT'] = merge['FLT'].str.replace(r'\d+', '', regex=True)
+    
     totaal_vluchten_per_luchthaven = merge.groupby('ICAO')['Airport ID'].value_counts()
 
     # Bereken het aantal vertraagde vluchten per maatschappij
@@ -85,7 +74,7 @@ def load_tab3():
     ratio_per_luchthaven['Ratio (%)'] = (ratio_per_luchthaven['Ratio'] * 100).round(2)
 
     unique_icao_orgdes_ratio = unique_icao_orgdes.merge(ratio_per_luchthaven, on = 'ICAO', how = 'left')
-
+    
     # kleur gedefinieerd op basis van ratio
     def get_delay_color(count):
         if count == 100:
@@ -202,7 +191,7 @@ def load_tab3():
         folium_map.get_root().header.add_child(folium.Element(script + css))
 
         return folium_map
-
+    
     #----------------------------------
     # Plot 1
     #-----------------------------------
@@ -226,7 +215,7 @@ def load_tab3():
                              colors = ['darkred', 'red', 'orange', 'darkgreen', 'green'],
                            labels = ['100%', '> 60%', '> 20%', '> 0%', '0%'])
         return m
-
+    
     # Roep de functie aan met de DataFrame als argument
     map_markers = load_markers(unique_icao_orgdes_ratio)
 
